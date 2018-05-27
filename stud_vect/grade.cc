828194f4d1acd49d8e49a25434d410cdd400694d
dio4 <dio4@yandex.ru>
2018-05-27 12:26:49 +0300

    Update grade.cc

--- stud_vect/grade.cc
+++ stud_vect/grade.cc
@@ -7,15 +7,13 @@
 using std::domain_error;  using std::vector;
 
 
-// compute a student's overall grade from midterm and final exam grades and homework grade
+// расчет общей оценки из промежуточных экзаменов, конечных и общей за ДЗ
 double grade(double midterm, double final, double homework)
 {
 	return 0.2 * midterm + 0.4 * final + 0.4 * homework;//здесь homework это значение double, высчитанное функ. median()
 }
 
-// compute a student's overall grade from midterm and final exam grades
-// and vector of homework grades.
-// this function does not copy its argument, because `median' does so for us.
+// расчет промежуточной и финальной оценки с добавлением ее в вектор hw 
 double grade(double midterm, double final, const vector<double>& hw)
 {
 	if (hw.size() == 0)
