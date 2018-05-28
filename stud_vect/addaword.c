00c17673041f1f830e4fdc717a0dfeeb866036fe
dio4 <dio4@yandex.ru>
2018-05-28 09:41:25 +0300

    Update addaword.c

--- stud_vect/addaword.c
+++ stud_vect/addaword.c
@@ -6,7 +6,7 @@
 int main(void)
 {
     FILE *fp;
-    char words[MAX] = "Moore 75 85 77 59 0 85 75 89";//задаем строку
+    char words[MAX] = "Alexander 75 85 77 59 0 85 75 89"; // задаем нужную строку
 //Функция fopen открывает файл, указанный аргументом filename с режимом доступа, указанным в аргументе mode и связывает с открытым файлом поток данных. 
     if ((fp = fopen("test10000", "w+")) == NULL)
     {
@@ -14,7 +14,7 @@ int main(void)
         exit(1);
     }
 
-    rewind(fp);           /* go back to beginning of file */
+    rewind(fp);           /* вернуться в начало файла */
 
     for ( int i = 0; i < 10000; i++)
     {
